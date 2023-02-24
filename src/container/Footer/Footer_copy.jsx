d2b654833f9f0941b324538132e3cc518c7b29fd
Mara <santamaria.ortiz.tamara@gmail.com>
2023-02-24 13:49:24 +0100

    Footer ended and responsive

--- src/container/Footer/Footer_copy.jsx
+++ src/container/Footer/Footer_copy.jsx
@@ -1,14 +1,15 @@
-import React from 'react';
-import { images } from "../../constants";
-import { FooterOverlay, Newsletter } from "../../components/";
+import React from "react";
 import { FiFacebook, FiTwitter, FiInstagram } from "react-icons/fi";
 
-import './Footer.css';
+import { FooterOverlay, Newsletter } from "../../components";
+import { images } from "../../constants";
+import "./Footer.css";
 
 const Footer = () => (
   <div className="app__footer section__padding" id="login">
     <FooterOverlay />
     <Newsletter />
+
     <div className="app__footer-links">
       <div className="app__footer-links_contact">
         <h1 className="app__footer-headtext">Contact Us</h1>
@@ -16,17 +17,18 @@ const Footer = () => (
         <p className="p__opensans">+1 212-344-1230</p>
         <p className="p__opensans">+1 212-555-1230</p>
       </div>
+
       <div className="app__footer-links_logo">
-        <img src={images.gericht} alt="logo" />
+        <img src={images.gericht} alt="footer_logo" />
         <p className="p__opensans">
-          "The best way to find yourself is to lose yourself in the service of
-          others.”
+          &quot;The best way to find yourself is to lose yourself in the service
+          of others.&quot;
         </p>
         <img
           src={images.spoon}
-          alt="spoon logo"
-          className="spoon__image"
+          className="spoon__img"
           style={{ marginTop: 15 }}
+          alt='spoon'
         />
         <div className="app__footer-links_icons">
           <FiFacebook />
@@ -34,16 +36,18 @@ const Footer = () => (
           <FiInstagram />
         </div>
       </div>
+
       <div className="app__footer-links_work">
         <h1 className="app__footer-headtext">Working Hours</h1>
         <p className="p__opensans">Monday-Friday:</p>
-        <p className="p__opensans">08:00 am -12:00 am</p>
+        <p className="p__opensans">08:00 am - 12:00 am</p>
         <p className="p__opensans">Saturday-Sunday:</p>
-        <p className="p__opensans">07:00am -11:00 pm</p>
+        <p className="p__opensans">07:00 am - 11:00 pm</p>
       </div>
     </div>
+
     <div className="footer__copyright">
-      <p className="p__opensans">2023 Gerícht. All Rights reserved.</p>
+      <p className="p__opensans">2021 Gericht. All Rights reserved.</p>
     </div>
   </div>
 );
